8e8656fae2e2a9e42b98a4079df823f89e590cfd
Jasiek Kostecki <bialykostek@gmail.com>
2022-10-02 21:26:34 +0200

    map

---
+++ webapp/js/map.js
@@ -0,0 +1,11 @@
+function mapInit(){
+    map = new OpenLayers.Map("mapDiv");
+    var mapnik         = new OpenLayers.Layer.OSM();
+    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
+    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
+    var position       = new OpenLayers.LonLat(13.41,52.52).transform( fromProjection, toProjection);
+    var zoom           = 15; 
+
+    map.addLayer(mapnik);
+    map.setCenter(position, zoom );
+}
